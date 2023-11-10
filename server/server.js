3e02a64c532c47ae9c98f0521b27533a1b7ef12d
Yiseo Kwon <yiseo1122@gmail.com>
2023-11-10 13:20:14 -0600

    docs: Deleted the unnecessary files and code

--- server/server.js
+++ server/server.js
@@ -19,9 +19,7 @@ const PORT = process.env.PORT || 3001;
 const startApolloServer = async () => {
   await server.start();
 
-  app.use(express.urlencoded({
-    extended: false
-  }));
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
   app.use('/graphql', expressMiddleware(server, {
@@ -30,10 +28,6 @@ const startApolloServer = async () => {
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
   }
 
   db.once('open', () => {
